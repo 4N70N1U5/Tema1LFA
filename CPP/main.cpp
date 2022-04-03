07eed37a936ccd383bbed0ee3b9e49779fd4e85a
4N70N1U5 <nitoi.antonio@gmail.com>
2022-04-03 16:52:47 +0300

    implemented reading data from input file

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,13 +1,58 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
-char adiacenta[50][50];
+std::ifstream fin("input.txt");
+std::ofstream fout("output.txt");
 
-void dfs()
+int n, m, nf, ni; // n - nr noduri; m - nr tranzitii; nf - nr stari finale; ni - nr input-uri
+char adiacenta[50][50]; // matrice de adiacenta
+int initiala; // indicele starii initiale
+std::vector<int> finale; // indicii starilor finale
+std::vector<std::string> cuv; // cuvintele care trebuie testate
+std::vector<int> drum; // nodurile din drumul gasit pentru fiecare cuvant
+bool acc; // starea cuvantului - acceptat sau nu
+
+void DFS()
 {
 
+}
+
+void citiri()
+{
+    fin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        char c;
+        fin >> a >> b >> c;
+        adiacenta[a][b] = c;
+    }
+
+    fin >> initiala;
+    
+    fin >> nf;
+
+    for (int i = 0; i < nf; i++)
+    {
+        int aux;
+        finale.push_back(aux);
+    }
+
+    fin >> ni;
+    
+    for (int i = 0; i < ni; i++)
+    {
+        std::string aux;
+        cuv.push_back(aux);
+    }
 }
 
 int main()
 {
 
+    std::cout << '\n';
+    return 0;
 }
